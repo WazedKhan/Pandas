34952839b2715f3d5c0f8f142e0a74e625557e65
WazedKhan <wazedkhan119399@gmail.com>
2024-05-02 23:42:46 +0600

    first solution

---
+++ modify_columns.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from icecream import ic
+
+data = {
+    "name": ["Jack", "Piper", "Mia", "Ulysses"],
+    "salary": [19666, 74754, 62509, 54866],
+}
+
+employees = pd.DataFrame(data)
+employees["salary"] = employees["salary"] * 2
+ic(employees)
