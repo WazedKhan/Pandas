f3707f3806f8529e7302e82d85b5f8c402ad9ce9
WazedKhan <wazedkhan119399@gmail.com>
2024-05-03 00:32:58 +0600

    pivot table

---
+++ pivot.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+data = {
+    "city": [
+        "Jacksonville",
+        "Jacksonville",
+        "Jacksonville",
+        "Jacksonville",
+        "Jacksonville",
+        "ElPaso",
+        "ElPaso",
+        "ElPaso",
+        "ElPaso",
+        "ElPaso",
+    ],
+    "month": [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+    ],
+    "temperature": [13, 23, 38, 5, 34, 20, 6, 26, 2, 43],
+}
+
+weather = pd.DataFrame(data)
+weather_pivoted = weather.pivot(index="month", columns="city", values="temperature")
+print(weather_pivoted)
